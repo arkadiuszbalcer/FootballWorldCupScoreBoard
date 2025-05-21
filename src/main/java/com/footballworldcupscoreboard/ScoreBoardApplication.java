c1c152e7957fea23bb284a6bb1ae98775635ce1f
Arkadiusz Balcer <arkadiuszbalcer92@gmail.com>
2025-05-21 16:15:45 +0200

    first commit

---
+++ src/main/java/com/footballworldcupscoreboard/ScoreBoardApplication.java
@@ -0,0 +1,9 @@
+package com.footballworldcupscoreboard;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ScoreBoardApplication {
+    public static void main(String[] args) {
+
+    }
+}
