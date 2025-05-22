b1cd34b0f90c06c05541e0c25e20eea81f3f21dc
Arkadiusz Balcer <arkadiuszbalcer92@gmail.com>
2025-05-22 19:12:07 +0200

    Added first test with initial matches and first function to check test assumptions

--- src/test/java/com/footballworldcupscoreboard/service/ScoreBoardTest.java
+++ src/test/java/com/footballworldcupscoreboard/service/ScoreBoardTest.java
@@ -1,7 +1,44 @@
 package com.footballworldcupscoreboard.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.footballworldcupscoreboard.model.Match;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 class ScoreBoardTest {
+private ScoreBoard scoreBoard;
+@BeforeEach
+    void setUp() {
+    scoreBoard = new ScoreBoard();
+}
+@Test
+    public void should_start_game_with_initial_Score_Zero_Zero() {
 
+    //given
+    List<Match> matchList = List.of(
+            new Match("Mexico", "Canada"),
+            new Match("Spain", "Brazil", 0, 0),
+            new Match("Germany", "France", 0, 0),
+            new Match("Uruguay", "Italy", 0, 0),
+            new Match("Argentina", "Australia", 0, 0)
+    );
+    for(Match match: matchList)
+        scoreBoard.startGame(match);
+
+    //when
+    List<Match>matches =scoreBoard.getAllMatches();
+
+    //Then
+    Match firstMatch = matches.getFirst();
+    assertThat(matchList).hasSize(5);
+    assertThat(firstMatch.getHomeTeam()).isEqualTo("Mexico");
+    assertThat(firstMatch.getAwayTeam()).isEqualTo("Canada");
+    assertThat(firstMatch.getHomeTeamScore()).isEqualTo(0);
+    assertThat(firstMatch.getAwayTeamScore()).isEqualTo(0);
+    }
 }
